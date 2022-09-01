ef4487222a5666e067e5f2e92e27831e79e75abb
JAbelaFanIsaac <112593979+JAbelaFanIsaac@users.noreply.github.com>
2022-09-01 08:56:56 +0800

    Add files via upload
    Naughty or Nice String Challenge

---
+++ PythonAS10.py
@@ -0,0 +1,55 @@
+gerdq = input("What is the string")
+
+fersa = gerdq.count("a")
+fsarg = gerdq.count("e")
+fhrsa = gerdq.count("i")
+fplra = gerdq.count("o")
+ferba = gerdq.count("u")
+webg = ferba + fersa + fhrsa + fplra + fsarg
+
+rsnk = gerdq.count("ab")
+rtdj = gerdq.count("cd")
+rhla = gerdq.count("xy")
+rezx = gerdq.count("pq")
+hpbk = rsnk + rtdj + rhla + rezx
+
+ao = gerdq.count("aa")
+fg = gerdq.count("bb")
+sa = gerdq.count("cc")
+fa = gerdq.count("dd")
+ba = gerdq.count("ee")
+fs = gerdq.count("ff")
+fh = gerdq.count("gg")
+ra = gerdq.count("hh")
+fe = gerdq.count("ii")
+rg = gerdq.count("jj")
+ga = gerdq.count("kk")
+pa = gerdq.count("ll")
+ea = gerdq.count("mm")
+jg = gerdq.count("nn")
+oa = gerdq.count("oo")
+za = gerdq.count("pp")
+fz = gerdq.count("qq")
+xg = gerdq.count("rr")
+la = gerdq.count("ss")
+ua = gerdq.count("tt")
+vg = gerdq.count("uu")
+kb = gerdq.count("vv")
+ie = gerdq.count("ww")
+xj = gerdq.count("xx")
+pm = gerdq.count("yy")
+bi = gerdq.count("zz")
+
+omg = bi + pm + xj + ie + kb + vg + ua + la + xg + fz
+why = oa + jg + ea + pa + ga + rg + fe + ra + fh + fs
+dididothis = ao + fg + sa + fa + ba + za
+
+ineedpoints = omg + why + dididothis
+
+if ineedpoints == 0:
+  if hpbk > 0:
+    if webg > 2:
+      print("Nice String")
+
+else:
+  print("Naughty String.")
