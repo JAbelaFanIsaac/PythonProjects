92e1d8597075356534b99d45b417da87d632f4ee
JAbelaFan1 <112593979+JAbelaFanIsaac@users.noreply.github.com>
2022-09-02 17:54:33 +0800

    Add files via upload

---
+++ AS06_VowelCounter.py
@@ -0,0 +1,8 @@
+ferv = input("Imput sentence in lowercase")
+gerdne = ferv.count("a")
+njryb = ferv.count("e")
+gewku = ferv.count("i")
+reote = ferv.count("o")
+vwedsa = ferv.count("u")
+freytse = gerdne + njryb + gewku +reote +vwedsa
+print("The sentence has", freytse, "vowels")
