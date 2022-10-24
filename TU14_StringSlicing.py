f1b48457228ecf6a9b961b49007ef0fa558e5b80
JAbelaFan1 <112593979+JAbelaFanIsaac@users.noreply.github.com>
2022-10-24 18:07:58 +0800

    Add files via upload

---
+++ TU14_StringSlicing.py
@@ -0,0 +1,16 @@
+MyString = "abcdef"
+print(MyString)
+print(MyString[0:3])
+print(MyString[3::])
+print(MyString[0:6:2])
+print(MyString[-1:-7:-1])
+print(MyString[0:-2])
+
+NewString = "qwertyuiop"
+print(NewString)
+print(NewString[0:3])
+print(NewString[3::])
+print(NewString[0:11:2])
+print(NewString[0:11:3])
+print(NewString[-1:-11:-1])
+print(NewString[0:-2])
