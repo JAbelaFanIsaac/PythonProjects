4557fc3cf8de3ffc9174142f16903cbf05c86d3a
JAbelaFan1 <112593979+JAbelaFanIsaac@users.noreply.github.com>
2024-02-22 08:52:55 +0800

    Create BinarySearchTree_words.py

---
+++ BinarySearchTree_words.py
@@ -0,0 +1,52 @@
+array = ["denial", "bargaining", "anger", "despair", "acceptance", "world", "pestillance"]
+
+class node:
+    def __init__(self, data):
+        self.__data = data
+        self.__leftChild = None
+        self.__rightChild = None
+
+    def insert(self, data):
+        if data < self.__data:
+            if self.__leftChild:
+                self.__leftChild.insert(data)
+            else:
+                self.__leftChild = node(data)
+                return
+
+        else:
+            if self.__rightChild:
+                self.__rightChild.insert(data)
+            else:
+                self.__rightChild = node(data)
+                return
+
+    def search(self, val):
+        if val == self.__data:
+            return str(val) + " is found in BST"
+        elif val < self.__data:
+            if self.__leftChild:
+                return self.__leftChild.search(val)
+            else:
+                return str(val)+ " not found in BST"
+        else:
+            if self.__rightChild:
+                return self.__rightChild.search(val)
+            else:
+                return str(val)+ " not found in BST"
+
+    def PrintTree(self):
+        if self.__leftChild:
+            self.__leftChild.PrintTree()
+        print(self.__data)
+        if self.__rightChild:
+            self.__rightChild.PrintTree()
+
+root = node("tired")
+for i in range(7):
+    root.insert(array[i])
+
+root.PrintTree()
+
+print(root.search("hello"))
+print(root.search("world"))
